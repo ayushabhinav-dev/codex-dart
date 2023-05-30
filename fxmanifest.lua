65669ac1f2b6d64d211d5fb9f80a04e7872ab0ce
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-05-29 19:11:50 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'TheStoicBear'
+description 'D.A.R.T - Direct Automotive Receiver & Transmitter'
+version '1.0.0'
+-- Resource Manifest
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+client_script 'client.lua'
+server_script 'server.lua'
+-- Add any other files, data files, or dependencies your resource requires
+
